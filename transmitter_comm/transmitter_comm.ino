82cd0974f139e3b11fa1a35a965b2b216256fe53
johnytripidakis <johnytrip@gmail.com>
2023-03-31 01:24:00 +0300

    more refactoring

--- transmitter_comm/transmitter_comm.ino
+++ transmitter_comm/transmitter_comm.ino
@@ -1,3 +1,4 @@
+const int maxSize = 62;
 const int dl = 9; // transmission delay per bit
 int led = 9;      // define transmission LED pin
 String input;     // user input message
@@ -10,13 +11,16 @@ void setup()
 {
     Serial.begin(9600);   // set baud rate to 9600
     pinMode(led, OUTPUT); // set pin 9 as output pin
-    for (int i = 0; i <= 2; i += 2)
-    {                       //---------- H E A D E R ----------
-        message[i] = 255;   // set first 5 table cells with the 5 bits
-        message[i + 1] = 0; // start header
-    }                       // 255 === max brightness, 0 === LED is off
-    message[4] = 255;       // 255 === 1 (bit value), 0 === 0
-} //---------- H E A D E R ----------
+
+    //---------- H E A D E R ----------
+    // 255 === max brightness, 0 === LED is off
+    message[0] = 255;       
+    message[1] = 0;       
+    message[2] = 255;       
+    message[3] = 0;       
+    message[4] = 255;
+         
+}
 
 void loop()
 {
@@ -30,16 +34,13 @@ void loop()
     newSize = input.length();    // store message length
 
     // if message is too long
-    if (newSize > 62)
+    if (newSize > maxSize)
     {
-        Serial.println("I received: " + input);
-        Serial.print(newSize);
-        Serial.println(" characters, message too big (max: 31 characters)");
+        Serial.println("I received: " + input + "\n" + newSize + " characters, message too big (max: 31 characters)");
         return;
     }
 
-    Serial.println("sending...");           // informe user that transmission has started
-    Serial.println("I received: " + input); // print the received input
+    Serial.println("sending...\nI received: " + input);
 
     pos = 5;             // reset position to 5 (the first 5{0,1,2,3,4} cells of the table are preset with the header)
     analogWrite(led, 0); // reset LED
@@ -52,7 +53,6 @@ void loop()
     // it will be converted to: 000011
     for (int i = 5; i >= 0; i--)
     {
-
         int inputBit = bitRead(input.length(), i);
         message[pos] = inputBit == 1 ? 255 : 0;
         pos++;
@@ -86,5 +86,5 @@ void loop()
         delay(dl);
     }
 
-    Serial.println("message sent"); // inform user that message has been sent
+    Serial.println("message sent");
 }
